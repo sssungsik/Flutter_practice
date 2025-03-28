7c77ab261d9f66ddcb998f90f08bf1c4e5b50778
sssungsik <sungsik97@naver.com>
2025-03-28 16:53:16 +0900

    stateful widget=>lifeCycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/screen/home.dart';
 import 'package:flutter_practice/screen/padding.dart';
+import 'package:flutter_practice/screen/statefulWidget_lifeCycle.dart';
 
 void main() {
 
@@ -10,7 +11,7 @@ void main() {
   // 플러터 실행
   runApp(
     MaterialApp(
-      home: PaddingEx(),
+      home: LifeCycleSample(),
     )
   );
 }
--- lib/screen/statefulWidget_lifeCycle.dart
+++ lib/screen/statefulWidget_lifeCycle.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 // 라이프 사이클
 
 class LifeCycleSample extends StatefulWidget {
+  final Color color;
   // 1. Constructor()
   // 2. createState()
-  const LifeCycleSample({super.key});
+  LifeCycleSample({
+    super.key,
+    required this.color,
+  }) {
+    print('1. Constructor');
+  }
+
 
   @override
-  State<LifeCycleSample> createState() => _LifeCycleSampleState();
+  State<LifeCycleSample> createState() {
+    print('2. createState');
+    return _LifeCycleSampleState();
+  }
 }
 
 class _LifeCycleSampleState extends State<LifeCycleSample> {
@@ -20,8 +29,62 @@ class _LifeCycleSampleState extends State<LifeCycleSample> {
   // 7. clean -> !! setState() !! -> 5.dirty()
   // 8. deactivate()
   // 9. dispose()
+
+  Color color = Colors.pinkAccent;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print('3. initState');
+    super.initState();
+  }
+
+
+  bool show = false;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print('5. build');
+    return Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (show)
+              GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      color = color == Colors.pinkAccent ? Colors.amberAccent : Colors.blueAccent;
+                    });
+                  },
+                  child: Container(color: widget.color, width: 50, height: 50)),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  show = !show;
+                });
+                print('클릭함');
+              },
+              child: Text('클릭'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    print('deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('dispose');
+    super.dispose();
   }
 }
